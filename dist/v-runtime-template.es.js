32406ac52fa308bb1beada7848237a9b0276371a
AndrejHafner <andrej.hafn3r@gmail.com>
2019-03-27 10:31:10 +0100

    Fix for methods

--- dist/v-runtime-template.es.js
+++ dist/v-runtime-template.es.js
@@ -48,8 +48,8 @@ var index = {
       //build new objects by removing keys if already exists (e.g. created by mixins)
       Object.keys($data).forEach(function (e) {if(typeof this$1.$data[e]==="undefined") { passthrough.$data[e] = $data[e]; }} );
       Object.keys($props).forEach(function (e) {if(typeof this$1.$props[e]==="undefined") { passthrough.$props[e] = $props[e]; }} );
-      Object.keys(methods).forEach(function (e) {if(typeof this$1.$options.methods==="undefined"||this$1.$options.methods[e]==="undefined") { passthrough.methods[e] = methods[e]; }} );
-      Object.keys(computed).forEach(function (e) {if(typeof this$1.$options.computed==="undefined"||this$1.$options.computed[e]==="undefined") { passthrough.computed[e] = computed[e]; }} );
+      Object.keys(methods).forEach(function (e) {if(typeof this$1.$options.methods !== "undefined" && this$1.$options.methods[e] !== "undefined") { passthrough.methods[e] = methods[e]; }} );
+      Object.keys(computed).forEach(function (e) {if(typeof this$1.$options.computed !== "undefined" && this$1.$options.computed[e] !== "undefined") { passthrough.computed[e] = computed[e]; }} );
       Object.keys(components).forEach(function (e) {if(typeof this$1.$options.components[e]==="undefined") { passthrough.components[e] = components[e]; }} );
 
       var methodKeys = Object.keys(passthrough.methods || {});
